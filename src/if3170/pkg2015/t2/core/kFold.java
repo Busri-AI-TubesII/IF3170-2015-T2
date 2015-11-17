e3065b6c1245e8e56ff17f2aa867612b3520d6ae
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-11-17 22:39:54 +0700

    kFold design

--- src/if3170/pkg2015/t2/core/kFold.java
+++ src/if3170/pkg2015/t2/core/kFold.java
@@ -5,6 +5,9 @@
  */
 package if3170.pkg2015.t2.core;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * @param T
  * @author nim_13512501
@@ -24,9 +27,73 @@ public class kFold implements OfflineLearningNominalDataClassifier {
         this.classifier=X.classifier.copy();
     }
     
+    
+    static void shuffleTrainingSet(int[][] inputArr, int[] outArr)
+    {
+        // If running on Java 6 or older, use `new Random()` on RHS here
+        Random rnd = new Random();
+        for (int i = inputArr.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int [] a = inputArr[index];
+            inputArr[index] = inputArr[i];
+            inputArr[i] = a;
+            int b = outArr[index];
+            outArr[index]=outArr[i];
+            outArr[i]=b;
+        }
+    }
+    
+    private double calculateAccuracy(OfflineLearningNominalDataClassifier cl,
+            int [][] inputCategory, int [] OutputClass) throws Exception{
+        int numFalse = 0;
+        int numTrue = 0;
+        for (int i=0;i<inputCategory.length;i++){
+            if (cl.predict(inputCategory[i])==OutputClass[i])
+                numTrue++;
+            else
+                numFalse++;
+        }
+        return ((double)numTrue/(double)(numFalse+numTrue));
+    }
+
     @Override
     public void train(int[] numInputCategory, int[][] inputCategory, int numOutputClass, int[] outputClass) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        shuffleTrainingSet(inputCategory, outputClass);
+        OfflineLearningNominalDataClassifier[] candidate = new OfflineLearningNominalDataClassifier[k];
+        double [] accuracy = new double[k];
+        int selected = 0;
+        for (int i=0;i<k;i++){
+            candidate[i]=classifier.copy();
+            
+            //pisah set menjadi tr dan cv
+            ArrayList<int[]> trInputCategory = new ArrayList<int[]>();
+            ArrayList<Integer> trOutputClass = new ArrayList<Integer>();
+            ArrayList<int[]> cvInputCategory = new ArrayList<int[]>();
+            ArrayList<Integer> cvOutputClass = new ArrayList<Integer>();
+            for (int j=0;j<i*inputCategory.length/k;j++){
+                trInputCategory.add(inputCategory[j]);
+                trOutputClass.add(outputClass[j]);
+            }
+            for (int j=i*inputCategory.length/k;j<(i+1)*inputCategory.length/k;j++){
+                cvInputCategory.add(inputCategory[j]);
+                cvOutputClass.add(outputClass[j]);
+            }
+            for (int j=(i+1)*inputCategory.length/k;j<inputCategory.length;j++){
+                trInputCategory.add(inputCategory[j]);
+                trOutputClass.add(outputClass[j]);
+            }
+            candidate[i].train(numInputCategory, (int[][])trInputCategory.toArray(new int[trInputCategory.size()][]),
+                    numOutputClass, trOutputClass.stream().mapToInt(Integer::intValue).toArray());
+            accuracy[i]=calculateAccuracy(candidate[i],
+                    cvInputCategory.toArray(new int[cvInputCategory.size()][]),
+                            cvOutputClass.stream().mapToInt(Integer::intValue).toArray());
+            if (accuracy[i]>accuracy[selected])
+                selected=i;
+        }
+        classifier=candidate[selected];
+        
     }
 
     @Override
