daaaf44e033fac20a7920b63140dc72706a36ec5
David <davidkwan95@gmail.com>
2015-12-01 16:42:05 +0700

    Implemented write and load hypothesis

--- src/if3170/pkg2015/t2/core/OfflineLearningNominalDataClassifier.java
+++ src/if3170/pkg2015/t2/core/OfflineLearningNominalDataClassifier.java
@@ -7,6 +7,7 @@ package if3170.pkg2015.t2.core;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Scanner;
 
 /**
  * sebuah OfflineLearningNominalDataClassifier.
@@ -52,5 +53,5 @@ public abstract class OfflineLearningNominalDataClassifier {
         return ((double)numTrue/(double)(numFalse+numTrue));
     }
     abstract public void writeHypothesis(OutputStream str);
-    abstract public void loadHypothesis(InputStream str);
+    abstract public void loadHypothesis(Scanner str);
 }
